fe489f9c8888e6401fd67c51ba0e5fee2427b2d1
kobayashiyabako16g <all.laisser.fair@gmail.com>
2025-05-04 14:27:13 +0900

    Update: reponse json struct

--- cmd/tiny/main.go
+++ cmd/tiny/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/kobayashiyabako16g/tiny-go/pkg/log"
 	"net/http"
 )
 
+type Response struct {
+	Status  string `json:"status"`
+	Message string `json:"message"`
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
-	log.Logger.Info("Hello, World")
+
+	res := Response{
+		Status:  "ok",
+		Message: "Hello, World",
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		log.Logger.Error("Failed to write response", "error", err)
+		http.Error(w, "Failed to write response", http.StatusInternalServerError)
+		return
+	}
 }
 
 func main() {
